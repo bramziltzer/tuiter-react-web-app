f1a08399074e4e9e6497c6ada356fe664bf047ca
Bram Ziltzer <bziltzer@gmail.com>
2023-02-19 21:27:06 -0500

    fix bottom margin

--- public/tuiter/ExploreScreen/index.js
+++ public/tuiter/ExploreScreen/index.js
@@ -4,7 +4,7 @@ import WhoToFollowList from "../WhoToFollowList/index.js";
 import ExploreComponent from "./ExploreComponent.js";
 function exploreContainer() {
     $('#wd-explore').append(`
-  <div class="row mt-2">
+  <div class="row mt-2 mb-2">
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
    <!--<h3>Navigation Sidebar</h3>-->
     ${NavigationSidebar('explore')}
--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -4,7 +4,7 @@ import PostSummaryList from "../PostSummaryList/index.js";
 import PostList from "../PostList/index.js";
 function homeContainer() {
     $('#wd-home').append(`
-  <div class="row mt-2">
+  <div class="row mt-2 mb-2">
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
    <!--<h3>Navigation Sidebar</h3>-->
     ${NavigationSidebar('home')}
